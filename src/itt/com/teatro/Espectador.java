93a122fb8d5236cbdeba9ec7416192c6db09e1b0
hannah-bannanah <Hannah-bannanah@users.noreply.github.com>
2021-02-18 12:23:53 +0100

    añadido getters/setters a Espectador

--- src/itt/com/teatro/Espectador.java
+++ src/itt/com/teatro/Espectador.java
@@ -24,6 +24,55 @@ public class Espectador {
 		this.edad = edad;
 	}
 	
+	
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	/**
+	 * @return the tlf
+	 */
+	public String getTlf() {
+		return tlf;
+	}
+
+
+	/**
+	 * @param tlf the tlf to set
+	 */
+	public void setTlf(String tlf) {
+		this.tlf = tlf;
+	}
+
+
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
 	/**
 	 * TODO
 	 * @return
