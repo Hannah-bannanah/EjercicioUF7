27241ebfd3389bf948fe3dc892c8409ce3dacc6d
hannah-bannanah <Hannah-bannanah@users.noreply.github.com>
2021-02-18 12:22:40 +0100

    creado la clase Obra con getters/setters y toString

---
+++ src/itt/com/teatro/Obra.java
@@ -0,0 +1,63 @@
+package itt.com.teatro;
+
+public class Obra {
+	private String titulo;
+	private String genero;
+	private int minutosDuracion;
+	
+	/**
+	 * Constructor de la clase Obra
+	 * @param titulo
+	 * @param genero
+	 * @param minutosDuracion
+	 */
+	public Obra(String titulo, String genero, int minutosDuracion) {
+		this.titulo = titulo;
+		this.genero = genero;
+		this.minutosDuracion = minutosDuracion;
+	}
+	
+	/**
+	 * @return el título de la obra
+	 */
+	public String getTitulo() {
+		return this.titulo;
+	}
+	/**
+	 * @param titulo el título de la obra
+	 */
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	/**
+	 * @return el género de la obra
+	 */
+	public String getGenero() {
+		return this.genero;
+	}
+	/**
+	 * @param genero el género de la obra
+	 */
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	/**
+	 * @return la duración de la obra en minutos
+	 */
+	public int getMinutosDuracion() {
+		return this.minutosDuracion;
+	}
+	/**
+	 * @param minutos
+	 */
+	public void setMinutosDuracion(int minutos) {
+		this.minutosDuracion = minutos;
+	}
+	
+	@Override
+	public String toString() {
+		return "Obra:\nTítulo: " + this.titulo + "\nGénero: " + this.genero + "\nDuración: " + this.minutosDuracion + " minutos.";
+	}
+}
